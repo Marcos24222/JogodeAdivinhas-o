0ad1ebbad6ddea73eafe4d7f3e9f4a8cbbd3d41d
Marcos24222 <marcos.almeida23@escola.pr.gov.br>
2026-07-30 15:15:18 +0000

    me perdi na progamação de novo

---
+++ jogodaforca.py
@@ -0,0 +1,15 @@
+#Jogo da forca 
+print("********************************")
+print("Bem-vindo ao Jogo da forca")
+print("********************************")
+
+palavrasecreta= "forte"
+enforcou = False
+acertou = False
+
+while(not enforcou and not acertou):
+    print("jogando")
+
+    print("jogando")
+
+    
