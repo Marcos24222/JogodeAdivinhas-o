5f746becd71545500b1630894d20cb3f07565942
Marcos24222 <marcos.almeida23@escola.pr.gov.br>
2026-08-06 15:10:44 +0000

    amanham é um novo dia com outros problemas

--- jogodaforca.py
+++ jogodaforca.py
@@ -1,15 +1,31 @@
-#Jogo da forca 
-print("********************************")
-print("Bem-vindo ao Jogo da forca")
-print("********************************")
+# Jogo da forca
+print("*********************************")
+print("Bem vindo ao jogo da Forca")
+print("*********************************")
 
-palavrasecreta= "forte"
+palavrasecreta = "Abacaxi".upper()
+letrasacertadas = ["_"] * len(palavrasecreta)
+
+print(palavrasecreta)
+
 enforcou = False
 acertou = False
+tentativas=0
 
-while(not enforcou and not acertou):
+while (not enforcou and not acertou and tentativas < 5):
+    chute = input("Digite uma letra? ")
+    chute = chute.strip()
+
+    if(chute in palavrasecreta):
+        index = 0
+        for letra in palavrasecreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+    else:
+        tentativas+=1
+        
+
     print("jogando")
 
-    print("jogando")
-
-    
+print("Fim do jogo")
